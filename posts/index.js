1128d76c1c09e0074762e9d49ef6d6862066d6e9
TarasPyzo <tyro.asap@gmail.com>
2021-09-19 17:12:54 +0300

    Implement posts service api

---
+++ posts/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+const PORT = 4000;
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  posts[id] = { id, title: req.body.title };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(PORT, () => console.log(`Posts service is listening on port ${PORT}`));
