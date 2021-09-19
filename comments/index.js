70d4084b812f9f15e9c403a90a8512461634c2a9
TarasPyzo <tyro.asap@gmail.com>
2021-09-19 17:51:02 +0300

    Implement comments service api

---
+++ comments/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+const PORT = 4001;
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const { id: postId } = req.params;
+  const comment = {
+    id: randomBytes(4).toString('hex'),
+    content: req.body.content,
+  };
+  const comments = commentsByPostId[postId] || [];
+  comments.push(comment);
+  commentsByPostId[postId] = comments;
+
+  res.status(201).send(comment);
+});
+
+app.listen(PORT, () => console.log(`Comments service is listening on port ${PORT}`));
