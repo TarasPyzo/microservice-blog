18362bb4d45c86f5aef2edc3b655dadf0ab3131b
TarasPyzo <tyro.asap@gmail.com>
2021-09-20 17:54:44 +0300

    Create custom event bus

---
+++ event-bus/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+const PORT = 4005;
+
+app.post('/events', (req, res) => {
+  const { event } = req.body;
+
+  axios.post('http://localhost:4000/events', { event });
+  axios.post('http://localhost:4001/events', { event });
+  axios.post('http://localhost:4002/events', { event });
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(PORT, () => console.log(`EVENT BUS is listening on port ${PORT}`));
