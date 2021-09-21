1d6ee58ad76952eb77c2c572c7218c88fe08e90d
TarasPyzo <tyro.asap@gmail.com>
2021-09-21 03:19:27 +0300

    Integrate event bus store

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,9 +5,11 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 const PORT = 4005;
+const events = [];
 
 app.post('/events', (req, res) => {
   const { event } = req.body;
+  events.push(event);
 
   axios.post('http://localhost:4000/events', { event });
   axios.post('http://localhost:4001/events', { event });
@@ -15,6 +17,10 @@ app.post('/events', (req, res) => {
   axios.post('http://localhost:4003/events', { event });
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send({ events });
 });
 
 app.listen(PORT, () => console.log(`EVENT BUS is listening on port ${PORT}`));
--- query/index.js
+++ query/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,10 +14,7 @@ app.get('/posts/comments', (req, res) => {
   res.send(posts);
 });
 
-app.post('/events', (req, res) => {
-  const { event } = req.body;
-  console.log(`[Query Service] ${event.type}`, event);
-
+const handleEvent = (event) => {
   if (event.type === 'PostCreated') {
     const { post } = event.data;
     posts[post.id] = post;
@@ -32,8 +30,21 @@ app.post('/events', (req, res) => {
     comment.status = event.data.comment.status;
     comment.content = event.data.comment.content;
   }
+};
+
+app.post('/events', (req, res) => {
+  const { event } = req.body;
+  console.log(`[Query Service] ${event.type}`);
+  handleEvent(event);
 
   res.send({});
 });
 
-app.listen(PORT, () => console.log(`Query service is listening on port ${PORT}`));
+app.listen(PORT, async () => {
+  console.log(`Query service is listening on port ${PORT}`);
+  const res = await axios.get('http://localhost:4005/events');
+  res.data.events.forEach(e => {
+    console.log(`Handle events ${e.type}`);
+    handleEvent(e);
+  });
+});
